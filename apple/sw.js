8d399f13aa74691149f66923bb1d7564f2a821ea
ddeock <119793245+ddeock@users.noreply.github.com>
2026-05-11 19:58:48 +0900

    Add files via upload

---
+++ apple/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'vibe-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
